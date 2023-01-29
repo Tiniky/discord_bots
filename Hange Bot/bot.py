9de5691589453466dc9ca340013f47f86a883655
Tiniky <99739442+Tiniky@users.noreply.github.com>
2023-01-29 19:09:10 +0100

    Update bot.py
    changed to pass the username to responses.py

--- Hange Bot/bot.py
+++ Hange Bot/bot.py
@@ -1,15 +1,15 @@
 import discord
 import responses
 
-async def send_message(message, user_message, is_private):
+async def send_message(message, user_message, is_private, author):
     try:
-        response = responses.handle_response(user_message)
+        response = responses.handle_response(user_message, author)
         await message.author.send(response) if is_private else await message.channel.send(response)
     except Exception as e:
         print(e)
 
 def run_discord_bot():
-    TOKEN = "" #the token of the bot from the Discord Developer page
+    TOKEN = 'MTA2NTcyOTY1MDQxMzgwNTcyOQ.GSaqGd.WW99x79ULnG7obqXK5sd1BspQy7vjCiSYtHdLM'
     intents = discord.Intents.default()
     intents.message_content = True
     client = discord.Client(intents=intents)
@@ -30,10 +30,10 @@ def run_discord_bot():
         channel = str(message.channel)
         print(f"{username} said: '{user_message}' ({channel})")
 
-        if user_message[0] == '?':
+        if user_message[0] == '>':
             user_message = user_message[1:]
-            await send_message(message, user_message, is_private=True)
+            await send_message(message, user_message, is_private=True, author=username)
         else:
-            await send_message(message, user_message, is_private=False)
+            await send_message(message, user_message, is_private=False, author=username)
 
     client.run(TOKEN)
