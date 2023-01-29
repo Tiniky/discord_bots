757de3781d772bad6f5b8afae45febe14a2cc8d6
Tiniky <99739442+Tiniky@users.noreply.github.com>
2023-01-29 19:13:38 +0100

    Update responses.py
    added decisions to help, da done

--- Hange Bot/responses.py
+++ Hange Bot/responses.py
@@ -2,6 +2,14 @@ import random
 
 def handle_response(msg) -> str:
     p_message = msg.lower()
+    users = []
+
+    with open('hange_users.json') as f:
+        users = json.load(f)
+        
+    usernames = []
+    for i in range(len(users)):
+        usernames.append(users[i]["username"])
 
     if p_message == 'helloo':
         return 'Oh hello there!'
@@ -25,17 +33,42 @@ def handle_response(msg) -> str:
         else:
             return "My final decision is: " + random.choice(messages_decide)
 
-    if p_message[0] == '!' and p_message[:5] != "!help":
+    if p_message[:9] == "decision" or p_message[:3] == "da" or p_message[:3] == "dr" or p_message[:3] == "dv" or p_message[:4] == "dea" or p_message[:4] == "dec" or p_message[:4] == "ded":
+        messages_decision = p_message.split("")
+        if messages_decision[0] == "decisionview" or messages_decision[0] == "dv":
+            if username not in usernames:
+                return "You have 0 saved decisions. Use '?decision'to learn more about those commands."
+            elif username in usernames:
+                for i in range(len(users)):
+                    if users[i]["username"] == username and len(users[i]["decisions"] == 0):
+                        return "You have 0 saved decisions. Use '?decision'to learn more about those commands."
+                    elif users[i]["username"] == username and len(users[i]["decisions"] == 1):
+                        return "Your saved decision: " + ' '.join(map(str, users[i]["decisions"]))
+                    else:
+                        if messages_decision[1] in users[i]["decisions"]:
+                            for j in range(len(users[i]["decisions"])):
+                                if users[i]["decisions"][j] == messages_decision[1]:
+                                    return "The options for the decision are: " + users[i]["options"][j][1].join(" or " + e for e in users[i]["options"][j][1:])
+
+    if p_message[0] == '!' and p_message != "!help":
         return "Uhm.. I might not be the best person to ask for that. But if you need a quick decision hit me up!"
 
     if p_message == '!help':
-        return "Use !help roll/rollxy/decide if you want to know more about them."
+        return "Use '?roll'/'?rollxy'/'?decide'/'?decision' if you want to know more about them."
 
-    if p_message[:5] == "!help" and len(p_message) > 5:
-        messages_help = p_message.split()
-        if messages_help[1] == "roll":
-            return "Use 'roll' and I'm gonna give you a random number."
-        elif messages_help[1] == "rollxy":
-            return "Use 'roll x y' and I'm gonna give you a number between x and y."
-        elif messages_help[1] == "decide":
-            return "Use 'decide x or y or ...' and I'm gonna pick one of your given options."
+    if p_message == "?roll":
+        return "Use 'roll' or 'r' and I'm gonna give you a random number."
+    elif p_message == "?rollxy":
+        return "Use 'roll x y' or 'r x y' and I'm gonna give you a number between x and y."
+    elif p_message == "?decide":
+        return "Use 'decide x or y or ...' or 'd x or y or ...' and I'm gonna pick one of your given options. If you added a frequent decision you can also use 'decide decision_name'."
+    elif p_message == "?decision":
+        return "I can remember frequent decisions. You can access them by 'decide decision_name'. Use '?decisionadd'/'?decisionedit'/'?decisionremove'/'?decisionview' if you want to know more about them."
+    elif p_message == "?decisionadd":
+        return "Use 'decisionadd(/da) name x or y or ...' and I'm gonna remember this decision."
+    elif p_message == "?decisionedit":
+        return "Use 'decisioneditadd(/dea) name x or ...' to add more options.  Use 'decisioneditchange(/dec) name option_before option_after' to change a saved option. Use 'decisioneditdelete(/ded) name option' to delete a saved option."
+    elif p_message == "?decisionremove":
+        return "Use 'decisionremove(/dr) name' to delete a saved decision."
+    elif p_message == "?decisionview":
+        return "Use 'decisionview' or 'dv' to view your saved decision. Use 'decisionview(/dv) name' to view the options of a saved decision."
