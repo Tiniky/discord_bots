5c24cdf761a2757fe0b21ce99ebd1010a387ccd2
Tiniky <99739442+Tiniky@users.noreply.github.com>
2023-02-01 22:09:20 +0100

    decide saved decision added

--- Hange Bot/responses.py
+++ Hange Bot/responses.py
@@ -1,8 +1,10 @@
 import random
+import json
 
-def handle_response(msg) -> str:
+def handle_response(msg, username) -> str:
     p_message = msg.lower()
     users = []
+    message_split = p_message.lower().split()
 
     with open('hange_users.json') as f:
         users = json.load(f)
@@ -14,32 +16,41 @@ def handle_response(msg) -> str:
     if p_message == 'helloo':
         return 'Oh hello there!'
     
-    if p_message == 'roll':
+    if p_message == "roll" or p_message == "r":
         return str(random.randint(1,1000))
 
-    messages_roll = p_message.split()
-    if len(messages_roll) == 3 and messages_roll[0] == "roll":
-        if messages_roll[1].isnumeric() and messages_roll[2].isnumeric():
-            return str(random.randint(int(messages_roll[1]),int(messages_roll[2])))
+    if len(message_split) == 3 and message_split[0] == "roll" or message_split[0] == "r":
+        if message_split[1].isnumeric() and message_split[2].isnumeric():
+            return str(random.randint(int(message_split[1]),int(message_split[2])))
         else:
             return 'Gimme two numbers next time!'
     
-    if p_message[:6] == "decide":
-        messages_decide = p_message[6:].split(" or ")
-        if len(messages_decide[0]) == 0:
+    if message_split[0] == "decide" or message_split[0] == "d":
+        options = " ".join(message_split[1:]).split(" or ")
+        print(options)
+        if options[0] == '':
             return 'Very funny.. you want me to decide or not?!'
-        if len(messages_decide) == 1:
-            return "You ain't giving me a choice here. Let's go with: " + messages_decide[0]
+        if len(options) == 1 and options[0] != '':
+            if username in usernames:
+                for i in range(len(usernames)):
+                    if users[i]["username"] == username:
+                        if options[0] in users[i]["decisions"]:
+                            for j in range(len(users[i]["decisions"])):
+                                if users[i]["decisions"][j] == options[0]:
+                                    return "My final decision is: " + random.choice(users[i]["options"][j])
+                        else:
+                            return "You ain't giving me a choice here. Let's go with: " + options[0]
+            else:
+                return "You ain't giving me a choice here. Let's go with: " + options[0]
         else:
-            return "My final decision is: " + random.choice(messages_decide)
+            return "My final decision is: " + random.choice(options)
 
-    messages_decisions = p_message.split()
-    if messages_decisions[0] == "decisionadd" or messages_decisions[0] == "da":
-        options = " ".join(messages_decisions[2:]).split(" or ")
-        if len(messages_decisions) >= 3:
+    if message_split[0] == "decisionadd" or message_split[0] == "da":
+        options = " ".join(message_split[2:]).split(" or ")
+        if len(message_split) >= 3:
             if username not in usernames:
                 decisions = []
-                decisions.append(messages_decisions[1])
+                decisions.append(message_split[1])
 
                 alloptions = []
                 alloptions.append(options)
@@ -52,10 +63,10 @@ def handle_response(msg) -> str:
             else:
                 for i in range(len(users)):
                     if users[i]["username"] == username:
-                        if messages_decisions[1] in users[i]["decisions"]:
+                        if message_split[1] in users[i]["decisions"]:
                            return "You already have a decision saved with that name. You can either edit it or give this another name."
                         else:
-                            users[i]["decisions"].append(messages_decisions[1])
+                            users[i]["decisions"].append(message_split[1])
                             users[i]["options"].append(options)
             
             with open('hange_users.json', 'w') as ff:
@@ -64,7 +75,7 @@ def handle_response(msg) -> str:
         else:
             return "Not enough parameters! Use '?decisions' to learn more about them."
 
-    if messages_decisions[0] == "decisionview" or messages_decisions[0] == "dv":
+    if message_split[0] == "decisionview" or message_split[0] == "dv":
         if username not in usernames:
             return "You have 0 saved decisions. Use '?decision' to learn more about those commands."
         elif username in usernames:
@@ -72,20 +83,20 @@ def handle_response(msg) -> str:
                 if users[i]["username"] == username:
                     if len(users[i]["decisions"]) == 0:
                         return "You have 0 saved decisions. Use '?decision' to learn more about those commands."
-                    elif len(messages_decisions) == 1:
+                    elif len(message_split) == 1:
                         return "Your saved decision(s): " + ' '.join(users[i]["decisions"])
                     else:
-                        if messages_decisions[1] in users[i]["decisions"]:
+                        if message_split[1] in users[i]["decisions"]:
                             for j in range(len(users[i]["decisions"])):
-                                if users[i]["decisions"][j] == messages_decisions[1]:
+                                if users[i]["decisions"][j] == message_split[1]:
                                     return "The options for the decision are: " + " or ".join(users[i]["options"][j])
                         else:
                             return "You don't have saved decision with that name. Use 'decisionview' or 'dv' to look at all your saved decisions."
 
-    if messages_decisions[0] == "decisionrename" or messages_decisions[0] == "dre":
-        if len(messages_decisions) == 3:
-            old_name = messages_decisions[1]
-            new_name = messages_decisions[2]
+    if message_split[0] == "decisionrename" or message_split[0] == "dre":
+        if len(message_split) == 3:
+            old_name = message_split[1]
+            new_name = message_split[2]
             if username not in usernames:
                 return "You don't have any saved decisions. Use '?decision' to learn more about those commands."
             elif username in usernames:
@@ -104,20 +115,20 @@ def handle_response(msg) -> str:
         else:
             return "Wrong parameters! Use '?decisions' to learn more about them."
 
-    if messages_decisions[0] == "decisionremove" or messages_decisions[0] == "dr":
-        if len(messages_decisions) == 2:
+    if message_split[0] == "decisionremove" or message_split[0] == "dr":
+        if len(message_split) == 2:
             if username not in usernames:
                 return "You don't have any saved decisions. Use '?decision' to learn more about those commands."
             elif username in usernames:
                 for i in range(len(users)):
                     if users[i]["username"] == username:
-                        if messages_decisions[1] not in users[i]["decisions"]:
+                        if message_split[1] not in users[i]["decisions"]:
                             return "You don't have a saved decision with that name. Use 'decisionview' or 'dv' to look at your saved decisions."
                         else:
                             decisions_new = []
                             options_new = []
                             for j in range(len(users[i]["decisions"])):
-                                if users[i]["decisions"][j] != messages_decisions[1]:
+                                if users[i]["decisions"][j] != message_split[1]:
                                     decisions_new.append(users[i]["decisions"][j])
                                     options_new.append(users[i]["options"][j])
                             
@@ -128,8 +139,6 @@ def handle_response(msg) -> str:
                             return "Okay, let's forget it."
         else:
             return "Wrong parameters! Use '?decisions' to learn more about them."
-        
-    #decisions in progress
 
     if p_message[0] == '!' and p_message != "!help":
         return "Uhm.. I might not be the best person to ask for that. But if you need a quick decision hit me up!"
@@ -142,9 +151,9 @@ def handle_response(msg) -> str:
     elif p_message == "?rollxy":
         return "Use 'roll x y' or 'r x y' and I'm gonna give you a number between x and y."
     elif p_message == "?decide":
-        return "Use 'decide x or y or ...' or 'd x or y or ...' and I'm gonna pick one of your given options. If you added a frequent decision you can also use 'decide decision_name'."
+        return "Use 'decide x or y or ...' or 'd x or y or ...' or 'decide saved_decision' and I'm gonna pick one of your given options. If you added a frequent decision you can also use 'decide decision_name'."
     elif p_message == "?decision":
-        return "I can remember frequent decisions. You can access them by 'decide decision_name'. Use '?decisionadd'/'?decisionedit'/'?decisionremove'/'?decisionview' if you want to know more about them."
+        return "I can remember frequent decisions. You can access them by 'decide decision_name'. Use '?decisionadd'/'?decisionrename'/'?decisionedit'/'?decisionremove'/'?decisionview' if you want to know more about them."
     elif p_message == "?decisionadd":
         return "Use 'decisionadd(/da) name x or y or ...' and I'm gonna remember this decision."
     elif p_message == "?decisionrename":
